9b2f03653e743aada5e2fdc1b36bb3e3419c992c
Amer Kamil Zainal <amer.kamil@gmail.com>
2013-07-13 15:07:07 +0000

    fixed typo in web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(messagebuf.toString('utf-8'));
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
